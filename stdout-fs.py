454f03f700ab94750bde2d095ea98029107ff570
Waldemar <waldemar.schwan@uswitch.com>
2016-05-04 10:03:01 +0100

    revert back to use the mem-fs methods
    simply falling back to the default methods results in errors

--- stdout-fs.py
+++ stdout-fs.py
@@ -27,14 +27,14 @@ class Stdout(LoggingMixIn, Operations):
         self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now,
                                st_mtime=now, st_atime=now, st_nlink=2)
 
-    #def chmod(self, path, mode):
-    #    self.files[path]['st_mode'] &= 0o770000
-    #    self.files[path]['st_mode'] |= mode
-    #    return 0
+    def chmod(self, path, mode):
+        self.files[path]['st_mode'] &= 0o770000
+        self.files[path]['st_mode'] |= mode
+        return 0
 
-    #def chown(self, path, uid, gid):
-    #    self.files[path]['st_uid'] = uid
-    #    self.files[path]['st_gid'] = gid
+    def chown(self, path, uid, gid):
+        self.files[path]['st_uid'] = uid
+        self.files[path]['st_gid'] = gid
 
     def create(self, path, mode):
         self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
@@ -44,37 +44,35 @@ class Stdout(LoggingMixIn, Operations):
         self.fd += 1
         return self.fd
 
-    #def getattr(self, path, fh=None):
-    #    if path not in self.files:
-    #        raise FuseOSError(ENOENT)
+    def getattr(self, path, fh=None):
+        if path not in self.files:
+            raise FuseOSError(ENOENT)
+        return self.files[path]
 
-    #    return self.files[path]
+    def getxattr(self, path, name, position=0):
+        attrs = self.files[path].get('attrs', {})
 
-    #def getxattr(self, path, name, position=0):
-    #    attrs = self.files[path].get('attrs', {})
+        try:
+            return attrs[name]
+        except KeyError:
+            return ''       # Should return ENOATTR
 
-    #    try:
-    #        return attrs[name]
-    #    except KeyError:
-    #        return ''       # Should return ENOATTR
-
-    #def listxattr(self, path):
-    #    attrs = self.files[path].get('attrs', {})
-    #    return attrs.keys()
+    def listxattr(self, path):
+        attrs = self.files[path].get('attrs', {})
+        return attrs.keys()
 
     def mkdir(self, path, mode):
         self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
                                 st_size=0, st_ctime=time(), st_mtime=time(),
                                 st_atime=time())
-
         self.files['/']['st_nlink'] += 1
 
     def open(self, path, flags):
         self.fd += 1
         return self.fd
 
-    #def read(self, path, size, offset, fh):
-    #    return self.data[path][offset:offset + size]
+    def read(self, path, size, offset, fh):
+        return self.data[path][offset:offset + size]
 
     def readdir(self, path, fh):
         return ['.', '..'] + [x[1:] for x in self.files if x != '/']
@@ -82,13 +80,13 @@ class Stdout(LoggingMixIn, Operations):
     def readlink(self, path):
         return self.data[path]
 
-    #def removexattr(self, path, name):
-    #    attrs = self.files[path].get('attrs', {})
+    def removexattr(self, path, name):
+        attrs = self.files[path].get('attrs', {})
 
-    #    try:
-    #        del attrs[name]
-    #    except KeyError:
-    #        pass        # Should return ENOATTR
+        try:
+            del attrs[name]
+        except KeyError:
+            pass        # Should return ENOATTR
 
     def rename(self, old, new):
         self.files[new] = self.files.pop(old)
@@ -97,32 +95,32 @@ class Stdout(LoggingMixIn, Operations):
         self.files.pop(path)
         self.files['/']['st_nlink'] -= 1
 
-    #def setxattr(self, path, name, value, options, position=0):
-    #    # Ignore options
-    #    attrs = self.files[path].setdefault('attrs', {})
-    #    attrs[name] = value
+    def setxattr(self, path, name, value, options, position=0):
+        # Ignore options
+        attrs = self.files[path].setdefault('attrs', {})
+        attrs[name] = value
 
-    #def statfs(self, path):
-    #    return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
+    def statfs(self, path):
+        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
 
-    #def symlink(self, target, source):
-    #    self.files[target] = dict(st_mode=(S_IFLNK | 0o777), st_nlink=1,
-    #                              st_size=len(source))
+    def symlink(self, target, source):
+        self.files[target] = dict(st_mode=(S_IFLNK | 0o777), st_nlink=1,
+                                  st_size=len(source))
 
-    #    self.data[target] = source
+        self.data[target] = source
 
-    #def truncate(self, path, length, fh=None):
-    #    self.data[path] = self.data[path][:length]
-    #    self.files[path]['st_size'] = length
+    def truncate(self, path, length, fh=None):
+        self.data[path] = self.data[path][:length]
+        self.files[path]['st_size'] = length
 
     def unlink(self, path):
         self.files.pop(path)
 
-    #def utimens(self, path, times=None):
-    #    now = time()
-    #    atime, mtime = times if times else (now, now)
-    #    self.files[path]['st_atime'] = atime
-    #    self.files[path]['st_mtime'] = mtime
+    def utimens(self, path, times=None):
+        now = time()
+        atime, mtime = times if times else (now, now)
+        self.files[path]['st_atime'] = atime
+        self.files[path]['st_mtime'] = mtime
 
     def write(self, path, data, offset, fh):
         stdout.write(data)
