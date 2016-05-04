4e616d21ca51276c663b1d9741fdfa437848a973
Waldemar <waldemar.schwan@uswitch.com>
2016-05-04 10:43:45 +0100

    bump python lib version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='stdout-fs',
-      version='1.1.1',
+      version='1.1.2',
       description='FUSE file system that forwards all write to all files to stdout.',
       author='Waldemar Schwan',
       author_email='waldemar.schwan@gmail.com',
